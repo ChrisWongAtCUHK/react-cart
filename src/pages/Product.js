1e8f7ee30ab4e676f90429cd78fb3c9f6b0a3c73
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-03 21:17:33 +0800

    Handle the refresh problem

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,37 +1,44 @@
-import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { selectProduct } from "../features/slices/productSlice"
-import { toCurrency } from "../utils"
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchAll, selectProduct } from '../features/slices/productSlice'
+import { toCurrency } from '../utils'
 
 function Product() {
-  const {id} = useParams()
+  const { id } = useParams()
   const items = useSelector(selectProduct).items
-  const product = items[id]
-  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (items.length === 0) {
+      dispatch(fetchAll())
+    }
+  }, [items, dispatch])
+
+  if (items.length === 0) {
+    return <h1> Loading </h1>
+  }
+
   return (
-    <div className="p-4 max-w-4xl mx-auto">
-    <div className="card lg:card-side bordered">
-      <figure className="px-10 pt-10">
-        <img
-          src={product.image}
-          alt="Product"
-          className="object-contain w-full h-64"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{product.title}</h2>
-        <p>{product.description}</p>
-        <p className="mt-4 text-lg">
-          { toCurrency(product.price) }
-        </p>
-        <div className="card-actions">
-          <button className="btn btn-primary" >
-            Add to Cart
-          </button>
+    <div className='p-4 max-w-4xl mx-auto'>
+      <div className='card lg:card-side bordered'>
+        <figure className='px-10 pt-10'>
+          <img
+            src={items[id].image}
+            alt='Product'
+            className='object-contain w-full h-64'
+          />
+        </figure>
+        <div className='card-body'>
+          <h2 className='card-title'>{items[id].title}</h2>
+          <p>{items[id].description}</p>
+          <p className='mt-4 text-lg'>{toCurrency(items[id].price)}</p>
+          <div className='card-actions'>
+            <button className='btn btn-primary'>Add to Cart</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
