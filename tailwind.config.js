61b53205b040acfc9e8f023c1ea66cabf0deda2f
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-24 12:44:29 +0800

    bumblebee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
-import daisyui from 'daisyui'
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    daisyui, // 直接使用 import 進來的變數
-  ],
-  daisyui: {
-    themes: ['bumblebee'],
-  },
+  plugins: [], // 這裡保持空白
 }
